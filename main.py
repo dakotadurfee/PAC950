cdea5be9182a9a97620b159ba55f3bb0f314da4c
dakotadurfee <ddurf11@wgu.edu>
2022-11-23 17:14:47 -0700

    commit

--- main.py
+++ main.py
@@ -42,9 +42,9 @@ def loadAddressData(fileName):
             i += 1
 
 
-loadPackageData('C:/Users/peral/PycharmProjects/PAC950/CSvs/package.csv')
-loadDistanceData('C:/Users/peral/PycharmProjects/PAC950/CSVs/distance.csv')
-loadAddressData('C:/Users/peral/PycharmProjects/PAC950/CSVs/address.csv')
+loadPackageData('C:/Users/dakot/PycharmProjects/PAC950/CSVs/package.csv')
+loadDistanceData('C:/Users/dakot/PycharmProjects/PAC950/CSVs/distance.csv')
+loadAddressData('C:/Users/dakot/PycharmProjects/PAC950/CSVs/address.csv')
 
 # print('Address:', addressData)
 
@@ -64,29 +64,20 @@ i = 0
 #           print('package Note:', column[1].getPackageNote())
 
 startingPoint = addressData[0]
-# truck.loadTruck(startingPoint, myHash)
+truck.loadTruck(startingPoint, myHash)
 
-# print('Truck 1:')
-# truck.getTruck1()
-# print('Truck 2:')
-# truck.getTruck2()
+print('Truck 1:')
+truck.getTruck1()
+print('Truck 2:')
+truck.getTruck2()
+#
+#print('Hash table:', myHash.getTable())
 
-# print('Distance:', truck.getTotalDistance())
+print('Distance:', truck.getTotalDistance())
 # print('truck 1 distances:', truck.getTruck1Distance())
 # print('truck 2 distances:', truck.getTruck2Distance())
 
-print('Empty: ', myHash.isEmpty())
-removeList = []
-for row in myHash.getTable():
-    for column in row:
-        removeList.append(column[0])
 
-for r in removeList:
-    myHash.remove(r)
-
-print(myHash.getTable())
-
-print('Empty:', myHash.isEmpty())
 
 # Finished: verified isEmpty() function works properly from hashTable class.
 # ToDo: implement isEmpty() function in loadTruck() while statement. Find a way to group packages that must be loaded together before load truck() executes.
--- truck.py
+++ truck.py
@@ -10,6 +10,8 @@ class Truck:
         self.truck2Distances = []
         self.distanceData = distanceData
         self.addressData = addressData
+        self.traveledT1 = 0
+        self.traveledT2 = 0
         self.totalDistance = 0
 
     def getDistanceBetween(self, address1, address2):
@@ -44,19 +46,35 @@ class Truck:
 
     def loadTruck(self, startingPoint, hashMap):
         start = startingPoint
+        buddyPackages = []
+        for row in hashMap.getTable():
+            for column in row:
+                note = column[1].getPackageNote()
+                if note[:22] == 'Must be delivered with':
+                    buddyPackages.append(column[1])
+                    if hashMap.search(note[23:25]) not in buddyPackages:
+                        buddyPackages.append(hashMap.search(note[23:25]))
+                    elif hashMap.search(note[26:]) not in buddyPackages:
+                        buddyPackages.append(hashMap.search(note[26:]))
 
-        while len(self.truck1Packages) < 16 and len(hashMap.getTable()) != 0:
+        for p in buddyPackages:
+            self.truck1Packages.append(p)
+            hashMap.remove(p.getPackageID())
+
+
+        while len(self.truck1Packages) < 16 and hashMap.isEmpty() != True:
             package = self.getMinDistance(start, hashMap.getTable())
-            if package.getPackageNote() == ' Can only be on truck 2':
-                package = self.getMinDistance(start, hashMap.getTable(), package)
-                self.truck1Packages.appen(package)
-                self.truck1Distances.append(self.getDistanceBetween(start, package.getPackageAddress()))
-                hashMap.remove(package.getPackageID())
-            elif package.getPackageNote() == 'Must be delivered with 13, 15' and len(self.truck1Packages) < 14:
-                package = hashMap.search(package.getPackageID())
+            if package.getPackageNote() == 'Can only be on truck 2':
+                while package.getPackageNote() == 'Can only be on truck 2':
+                    package = self.getMinDistance(start, hashMap.getTable(), package)
                 self.truck1Packages.append(package)
                 self.truck1Distances.append(self.getDistanceBetween(start, package.getPackageAddress()))
                 hashMap.remove(package.getPackageID())
+            elif package.getPackageNote() == 'Delayed on flight---will not arrive to depot until 9:05 am':
+                #TODO: create if statement using timedelta objects to see if package can be loaded
+                package = self.getMinDistance(start, hashMap.getTable(), package)
+                self.truck1Packages.append(package)
+                self.truck1Distances.append(self.getDistanceBetween(start, package.getPackageAddress()))
             else:
                 package = self.getMinDistance(start, hashMap.getTable(), package)
                 self.truck1Packages.append(package)
@@ -73,15 +91,18 @@ class Truck:
             hashMap.remove(package.getPackageID())
             start = self.truck2Packages[-1].getPackageAddress()
 
+    def deliveryPackages(self):
+
+
     def getTruck1(self):
         print('Length:', len(self.truck1Packages))
         for package in self.truck1Packages:
-            print('Package ID:', package.getPackageID())
+            print('Package Note:', package.getPackageNote())
 
     def getTruck2(self):
         print('Length:', len(self.truck2Packages))
         for package in self.truck2Packages:
-            print('Package ID:', package.getPackageID())
+            print('Package Note:', package.getPackageNote())
 
     def getTotalDistance(self):
         return self.totalDistance
